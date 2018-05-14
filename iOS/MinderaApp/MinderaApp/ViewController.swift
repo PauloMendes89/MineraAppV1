06af9766c92ee8e29a5c80117639f342a71c0800
Paulo Mendes <paulomendes89@hotmail.com>
2018-05-15 00:54:54 +0100

    first commit

---
+++ iOS/MinderaApp/MinderaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinderaApp
+//
+//  Created by Paulo Mendes on 13/05/2018.
+//  Copyright © 2018 SwitchCase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
